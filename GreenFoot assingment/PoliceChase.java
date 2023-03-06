6660ac8a98bcc170386f18d4c26340c8e0dc2c5c
49M <74563947+49M@users.noreply.github.com>
2023-03-06 13:26:49 -0500

    Day 1 startup
    - Opened the repository
    - Gave it a name
    - Started the program

---
+++ GreenFoot assingment/PoliceChase.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class PoliceChase here.
+ * 
+ * Michal Buczek 
+ *  V 0.1 Monday, March 6, 2023 
+ */
+public class PoliceChase extends World
+{
+    //Global Variables:
+    String gameState;
+    /**
+     * Constructor for objects of class PoliceChase.
+     * 
+     */
+    public PoliceChase()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}

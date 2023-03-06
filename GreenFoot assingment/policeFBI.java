d58993c8208dcddb4793cbe26fa2c475bcf268ac
49M <74563947+49M@users.noreply.github.com>
2023-03-06 15:00:59 -0500

    Day 1 additions - Characters
    - Added player car class
    - Added police car class
    - Added background class
    - Haven't started implementing these classes (I will hopefully start the implementation tomorrow

---
+++ GreenFoot assingment/policeFBI.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class policeFBI here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class policeFBI extends Actor
+{
+    /**
+     * Act - do whatever the policeFBI wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
